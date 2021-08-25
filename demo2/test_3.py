9d091a749c7b4959220375d159e18edfc1a08c63
amarn <amar.charkha4@gamil.com>
2021-08-25 21:00:25 +0530

    new test file demo3

---
+++ demo2/test_3.py
@@ -0,0 +1,12 @@
+import pytest
+import math
+
+def test_sqrt():
+    num = 9
+    assert math.sqrt(num)== 3
+def testMethod5():
+    print("test method 5 ")
+def testMethod8():
+    print("test method 8")
+def testMethod7():
+    print("test method7 ")
